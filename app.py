90b29b6c7f4b30874d3f9c83ec26be90d721ed7c
Matthijs Boet <m.boet@chello.nl>
2023-07-06 01:29:24 +0200

    working app in python

---
+++ app.py
@@ -0,0 +1,56 @@
+import openai
+from dotenv import dotenv_values
+import argparse
+
+config = dotenv_values(".env")
+openai.api_key = config["OPENAI_API_KEY"]
+
+def bold(text):
+    bold_start = "\033[1m"
+    bold_end = "\033[0m"
+    return bold_start + text + bold_end
+
+def blue(text):
+    blue_start = "\033[34m"
+    blue_end = "\033[0m"
+    return blue_start + text + blue_end
+
+def green(text):
+    green_start = '\033[92m'
+    green_end = "\033[0m"
+    return green_start + text + green_end
+
+def yellow(text):
+    yellow_start = "\033[43m"
+    yellow_end = "\033[0m"
+    return yellow_start + text + yellow_end
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Simple command line ChatGPT chatbot")
+
+    parser.add_argument("--personality", "--p", type=str, help="A brief summary of the chatbot's personality", default="friendly and helpful")
+
+    args = parser.parse_args()
+    print(bold(args.personality.capitalize()))
+
+    initial_prompt = f"You are a conversational chatbot. Your personality is: {args.personality}"
+    messages = [{"role": "system", "content": initial_prompt}]
+
+    while(True):
+        try: 
+            user_input = input(bold(blue("You: ")))
+            messages.append({"role": "user", "content": user_input})
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=messages
+            )
+            messages.append(response.choices[0].message.to_dict())
+            print(bold(yellow("Assistant:")), green(response.choices[0].message.content))
+            # print(messages)
+        except KeyboardInterrupt:
+            print("Exiting...")
+            break
+
+if(__name__ == "__main__"):
+    main()
